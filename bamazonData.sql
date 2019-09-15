50b06febb212ea17eeda666131d32db88d3c58bf
Luigi Cruz <angeloluigicruz@gmail.com>
2019-09-14 22:25:44 -0700

    submission

---
+++ bamazonData.sql
@@ -0,0 +1,30 @@
+-- Data used to populate tables
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone 11', 'Phones', 700.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone 11 Pro', 'Phones', 1000.00, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone XR (2018)', 'Phones', 650.00, 60);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('AirPods 2', 'Headphones', 160.00, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPad 64GB', 'Tablet', 350.00, 65);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPad Pro', 'Tablet', 500.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Apple Pencil', 'Accessories', 100.00, 75);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Macbook', 'Laptop', 1200.00, 14);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone Case', 'Accessories', 50.00, 350);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Lightning Cable', 'Accessories', 25.00, 1200);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Battery Pack', 'Accessories', 50.00, 180);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('iPhone 7 refurbished', 'Phones', 300.00, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Apple Watch Series 3', 'Smart Watch', 300.00, 125);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Apple Watch Series 4', 'Smart Watch', 500.00, 30);
+
